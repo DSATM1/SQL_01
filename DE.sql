f581d007ddfbf3fc023001368e1c98a389cb9d89
Suraj <surajsppokale@gmail.com>
2026-04-22 16:40:30 +0530

    DB created

---
+++ DE.sql
@@ -0,0 +1,37 @@
+create database DE;
+use  DE;
+
+-- CREATE TABLE customers (
+--     customer_id INT PRIMARY KEY,
+--     name VARCHAR(50)
+-- );
+
+-- INSERT INTO customers VALUES
+-- (1, 'Ravi'),
+-- (2, 'Priya'),
+-- (3, 'Arjun'),
+-- (4, 'Neha');
+
+-- create table orders (
+-- order_id int primary key,
+-- customer_id int,
+-- amount int 
+-- );
+
+-- insert into orders values
+-- (101, 1, 5000),
+-- (102, 1, 3000),
+-- (103, 2, 4000),
+-- (104, 2, 2000),
+-- (105, 3, 1000);
+
+-- select * from customers;
+
+-- select * from orders;
+
+-- select c.name, sum(o.amount), count(o.order_id) 
+-- from customers c 
+-- left join orders o 
+-- on c.customer_id = o.customer_id 
+-- group by c.name 
+-- having sum(o.amount) > 5000; 
