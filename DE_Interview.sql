4f9dd826be87e6b3ee4c36da453d7a1a42a548da
Suraj <surajsppokale@gmail.com>
2026-04-23 13:53:49 +0530

    Tables Inserted 1

--- DE_Interview.sql
+++ DE_Interview.sql
@@ -6,6 +6,12 @@ customer_id int primary key,
 name varchar(20)
 );
 
+insert into customers values
+(1, 'Ravi'),
+(2, 'Priya'),
+(3, 'Arjun'),
+(4, 'Neha');
+
 create table orders(
 order_id int primary key,
 customer_id int,
